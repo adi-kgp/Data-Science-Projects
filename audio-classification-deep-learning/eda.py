4603a5d670d90b8e84490648a6ff4f3513aec6a2
adi-kgp <john.claystone@gmail.com>
2024-03-17 16:51:50 +0530

    learnt different digital signal processing of audio files using librosa, python_speech_features packages, and visualized the signals using matplotlib, more research needs to be done by me to understand the fundamentals better

--- audio-classification-deep-learning/eda.py
+++ audio-classification-deep-learning/eda.py
@@ -8,14 +8,14 @@ from python_speech_features import mfcc, logfbank
 import librosa #common default audio library
 
 def plot_signals(signals):
-    fig, axes = plt.subplots(nrows=2, ncols=5, sharex=False, sharey=True, sharey=True, figsize=(20,5))
+    fig, axes = plt.subplots(nrows=2, ncols=5, sharex=False, sharey=True, figsize=(20,5))
     fig.suptitle('Time Series', size=16)
     i=0
     for x in range(2):
         for y in range(5):
             axes[x,y].set_title(list(signals.keys())[i])
             axes[x,y].plot(list(signals.values())[i])
-            axes[x,y].get_axis().set_visible(False)
+            axes[x,y].get_xaxis().set_visible(False)
             axes[x,y].get_yaxis().set_visible(False)
             i += 1
     
@@ -31,7 +31,7 @@ def plot_fft(fft):
             axes[x,y].set_title(list(fft.keys())[i])
             axes[x,y].plot(freq, Y)
             axes[x,y].get_xaxis().set_visible(False)
-            axes[x,y].get_yaxis().set_variable(False)
+            axes[x,y].get_yaxis().set_visible(False)
             i += 1
             
 def plot_fbank(fbank):
@@ -43,7 +43,7 @@ def plot_fbank(fbank):
             axes[x,y].set_title(list(fbank.keys())[i])
             axes[x,y].imshow(list(fbank.values())[i], cmap='hot', interpolation='nearest')
             axes[x,y].get_xaxis().set_visible(False)
-            axes[x,y].get_yaxis().set_variable(False)
+            axes[x,y].get_yaxis().set_visible(False)
             i += 1
 
 def plot_mfccs(mfccs):
@@ -55,9 +55,27 @@ def plot_mfccs(mfccs):
             axes[x,y].set_title(list(mfccs.keys())[i])
             axes[x,y].imshow(list(mfccs.values())[i], cmap='hot', interpolation='nearest')
             axes[x,y].get_xaxis().set_visible(False)
-            axes[x,y].get_yaxis().set_variable(False)
+            axes[x,y].get_yaxis().set_visible(False)
             i += 1
-            
+      
+def calc_fft(y, rate):
+    n = len(y)
+    freq = np.fft.rfftfreq(n, d=1/rate)
+    Y = abs(np.fft.rfft(y)/n)
+    return (Y, freq)
+
+def envelope(y, rate, threshold):
+    mask = []
+    y = pd.Series(y).apply(np.abs)
+    y_mean = y.rolling(window=int(rate/10), min_periods=1, center=True).mean()
+    for mean in y_mean:
+        if mean > threshold:
+            mask.append(True)
+        else:
+            mask.append(False)
+    return mask
+ 
+     
 df = pd.read_csv('instruments.csv')
 df.set_index('fname', inplace=True)
 
@@ -77,6 +95,41 @@ ax.axis('equal')
 plt.show()
 df.reset_index(inplace=True)
 
+signals = {}
+fft = {}
+fbank = {}
+mfccs = {}
+    
 for c in classes:
     wav_file = df[df.label==c].iloc[0,0]
-    signal, rate = librosa.load('wavfiles/'+wav_file, sr=44100)
+    signal, rate = librosa.load('wavfiles_combined/'+wav_file, sr=44100)
+    mask = envelope(signal, rate, threshold=0.0005)
+    signal = signal[mask]
+    signals[c] = signal
+    
+    fft[c] = calc_fft(signal, rate)
+    
+    bank = logfbank(signal[:rate], rate, nfilt=26, nfft=1103).T
+    fbank[c] = bank
+    
+    mel = mfcc(signal[:rate], rate, numcep=13, nfilt=26, nfft=1103).T
+    mfccs[c] = mel
+    
+plot_signals(signals)
+plt.show()
+
+plot_fft(fft)
+plt.show()
+
+plot_fbank(fbank)
+plt.show()
+
+plot_mfccs(mfccs)
+plt.show()
+
+if len(os.listdir('clean')) == 0:
+    for f in tqdm(df.fname):
+        signal, rate = librosa.load('wavfiles_combined/'+f, sr=16000)
+        mask = envelope(signal, rate, 0.0005)
+        wavfile.write(filename='clean/'+f, rate=rate, data=signal[mask])
+        
